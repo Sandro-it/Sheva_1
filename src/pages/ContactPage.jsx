32cf6b7d8b347a02058fbe607b529a2f1e479946
Sandro-it <kozachenkooleksandr@gmail.com>
2024-07-24 21:15:26 +0300

    label-form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,6 +3,96 @@ import { useForm, ValidationError } from "@formspree/react";
 import { useLocation } from "react-router-dom";
 import styles from "../styles/ContactPage.module.css";
 
+// const ContactPage = () => {
+//   const [state, handleSubmit] = useForm("xzzpdkyl");
+//   const [submitted, setSubmitted] = useState(false);
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     if (state.succeeded) {
+//       setSubmitted(true);
+//     }
+//   }, [state.succeeded]);
+
+//   useEffect(() => {
+//     setSubmitted(false);
+//   }, [location.key]);
+
+//   return (
+//     <div className={styles.contactPage}>
+//       {submitted ? (
+//         <p>Дякуємо за ваше повідомлення!</p>
+//       ) : (
+//         <>
+//           <h1>Контакти</h1>
+//           <p className={styles.text}>
+//             Тут ви можете знайти інформацію про процедуру отримання ліцензій та
+//             інші правові питання.
+//           </p>
+//           <form className={styles.contactForm} onSubmit={handleSubmit}>
+//             <label className={styles.label} htmlFor="name">
+//               Ім'я
+//             </label>
+//             <input
+//               className={styles.input}
+//               type="text"
+//               id="name"
+//               name="name"
+//               required
+//               autoComplete="name"
+//             />
+//             <label className={styles.label} htmlFor="email">
+//               Електронна пошта
+//             </label>
+//             <input
+//               className={styles.input}
+//               type="email"
+//               id="email"
+//               name="email"
+//               required
+//               autoComplete="email"
+//             />
+//             <ValidationError
+//               prefix="Email"
+//               field="email"
+//               errors={state.errors}
+//             />
+//             <label className={styles.label} htmlFor="message">
+//               Повідомлення
+//             </label>
+//             <textarea
+//               className={styles.textarea}
+//               id="message"
+//               name="message"
+//               required
+//               autoComplete="message"
+//             ></textarea>
+//             <ValidationError
+//               prefix="Message"
+//               field="message"
+//               errors={state.errors}
+//             />
+//             <button
+//               className={styles.button}
+//               type="submit"
+//               disabled={state.submitting}
+//             >
+//               Надіслати
+//             </button>
+//           </form>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ContactPage;
+
+import React, { useState, useEffect } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import { useLocation } from "react-router-dom";
+import styles from "../styles/ContactPage.module.css";
+
 const ContactPage = () => {
   const [state, handleSubmit] = useForm("xzzpdkyl");
   const [submitted, setSubmitted] = useState(false);
@@ -32,26 +122,26 @@ const ContactPage = () => {
           <form className={styles.contactForm} onSubmit={handleSubmit}>
             <label className={styles.label} htmlFor="name">
               Ім'я
+              <input
+                className={styles.input}
+                type="text"
+                id="name"
+                name="name"
+                required
+                autoComplete="name"
+              />
             </label>
-            <input
-              className={styles.input}
-              type="text"
-              id="name"
-              name="name"
-              required
-              autoComplete="name"
-            />
             <label className={styles.label} htmlFor="email">
               Електронна пошта
+              <input
+                className={styles.input}
+                type="email"
+                id="email"
+                name="email"
+                required
+                autoComplete="email"
+              />
             </label>
-            <input
-              className={styles.input}
-              type="email"
-              id="email"
-              name="email"
-              required
-              autoComplete="email"
-            />
             <ValidationError
               prefix="Email"
               field="email"
@@ -59,14 +149,14 @@ const ContactPage = () => {
             />
             <label className={styles.label} htmlFor="message">
               Повідомлення
+              <textarea
+                className={styles.textarea}
+                id="message"
+                name="message"
+                required
+                autoComplete="message"
+              ></textarea>
             </label>
-            <textarea
-              className={styles.textarea}
-              id="message"
-              name="message"
-              required
-              autoComplete="message"
-            ></textarea>
             <ValidationError
               prefix="Message"
               field="message"
