d43cb82491969a34c8f95d787024f78e798e808d
Sandro-it <kozachenkooleksandr@gmail.com>
2024-07-24 15:46:02 +0300

    CP-form-useffect

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import styles from "../styles/ContactPage.module.css";
 
