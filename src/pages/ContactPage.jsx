cc1ea262adf6b1f1a6b47125dd3ce62388019f79
Sandro-it <kozachenkooleksandr@gmail.com>
2024-07-24 15:42:43 +0300

    CP-form-clear-2

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -13,69 +13,73 @@ const ContactPage = () => {
   }, [state.succeeded]);
 
   useEffect(() => {
-    return () => {
-      setSubmitted(false);
-    };
+    setSubmitted(false);
   }, []);
-
-  if (submitted) {
-    return <p>Дякуємо за ваше повідомлення!</p>;
-  }
 
   return (
     <div className={styles.contactPage}>
-      <h1>Контакти</h1>
-      <p className={styles.text}>
-        Тут ви можете знайти інформацію про процедуру отримання ліцензій та інші
-        правові питання.
-      </p>
-      <form className={styles.contactForm} onSubmit={handleSubmit}>
-        <label className={styles.label} htmlFor="name">
-          Ім'я
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          id="name"
-          name="name"
-          required
-          autoComplete="name"
-        />
-        <label className={styles.label} htmlFor="email">
-          Електронна пошта
-        </label>
-        <input
-          className={styles.input}
-          type="email"
-          id="email"
-          name="email"
-          required
-          autoComplete="email"
-        />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <label className={styles.label} htmlFor="message">
-          Повідомлення
-        </label>
-        <textarea
-          className={styles.textarea}
-          id="message"
-          name="message"
-          required
-          autoComplete="message"
-        ></textarea>
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <button
-          className={styles.button}
-          type="submit"
-          disabled={state.submitting}
-        >
-          Надіслати
-        </button>
-      </form>
+      {submitted ? (
+        <p>Дякуємо за ваше повідомлення!</p>
+      ) : (
+        <>
+          <h1>Контакти</h1>
+          <p className={styles.text}>
+            Тут ви можете знайти інформацію про процедуру отримання ліцензій та
+            інші правові питання.
+          </p>
+          <form className={styles.contactForm} onSubmit={handleSubmit}>
+            <label className={styles.label} htmlFor="name">
+              Ім'я
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              id="name"
+              name="name"
+              required
+              autoComplete="name"
+            />
+            <label className={styles.label} htmlFor="email">
+              Електронна пошта
+            </label>
+            <input
+              className={styles.input}
+              type="email"
+              id="email"
+              name="email"
+              required
+              autoComplete="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <label className={styles.label} htmlFor="message">
+              Повідомлення
+            </label>
+            <textarea
+              className={styles.textarea}
+              id="message"
+              name="message"
+              required
+              autoComplete="message"
+            ></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <button
+              className={styles.button}
+              type="submit"
+              disabled={state.submitting}
+            >
+              Надіслати
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
