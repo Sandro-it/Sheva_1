97837e66a0f8466505c3c994e3580888915b69db
Sandro-it <kozachenkooleksandr@gmail.com>
2024-07-24 13:02:07 +0300

    upd-4

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@
 // export default App;
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
