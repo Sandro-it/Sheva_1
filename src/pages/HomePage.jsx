ebe9059bc3d1f11ccd996763c58b72ee10a7e2de
Sandro-it <kozachenkooleksandr@gmail.com>
2024-07-25 15:48:42 +0300

    home1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,32 @@
 import styles from "../styles/HomePage.module.css";
 
+// const HomePage = () => {
+//   return (
+//     <div className={styles.homePage}>
+//       <h1>Yuri Shevchenko</h1>
+//       <p className={styles.description}>
+//         «Я безмежно радію, коли бачу, що моя музика спромоглася викликати на
+//         обличчі слухача щасливу посмішку. А не менше я просто не згоден». Юрій
+//         Шевченко!
+//       </p>
+//       <img className={styles.photo} src="/images/Sheva1.jpg" alt="Композитор" />
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
-      <h1>Yuri Shevchenko</h1>
-      <p className={styles.description}>Коротка біографія композитора...</p>
+      <div>
+        <h1>Yuri Shevchenko</h1>
+        <p className={styles.description}>
+          «Я безмежно радію, коли бачу, що моя музика спромоглася викликати на
+          обличчі слухача щасливу посмішку. А не менше я просто не згоден». Юрій
+          Шевченко!
+        </p>
+      </div>
       <img className={styles.photo} src="/images/Sheva1.jpg" alt="Композитор" />
     </div>
   );
