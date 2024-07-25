db0db9b7b1af3b795d12e6cb9e1534a74b797b8e
Sandro-it <kozachenkooleksandr@gmail.com>
2024-07-25 18:38:56 +0300

    home3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,11 +20,10 @@ const HomePage = () => {
   return (
     <div className={styles.homePage}>
       <div>
-        <h1>Yuri Shevchenko</h1>
+        <h1>Юрій Шевченко</h1>
         <p className={styles.description}>
           «Я безмежно радію, коли бачу, що моя музика спромоглася викликати на
-          обличчі слухача щасливу посмішку. А на менше я просто не згоден». Юрій
-          Шевченко!
+          обличчі слухача щасливу посмішку. А на менше я просто не згоден».
         </p>
       </div>
       <img className={styles.photo} src="/images/Sheva1.jpg" alt="Композитор" />
