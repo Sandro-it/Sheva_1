c7594fcb8fd46ce773955e21cae9d0476e1d0f2c
Sandro-it <kozachenkooleksandr@gmail.com>
2024-08-05 11:35:29 +0300

    clog

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -4,6 +4,11 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { from_name, from_email, message } = req.body;
 
+    // Додаємо логування змінних середовища
+    console.log("Service ID:", process.env.EMAIL_SERVICE_ID);
+    console.log("Template ID:", process.env.EMAIL_TEMPLATE_ID);
+    console.log("User ID:", process.env.EMAIL_USER_ID);
+
     try {
       const result = await emailjs.send(
         process.env.EMAIL_SERVICE_ID,
@@ -17,6 +22,7 @@ export default async function handler(req, res) {
       );
       res.status(200).json({ success: true, result });
     } catch (error) {
+      console.error("Error sending email:", error); // Логування помилки
       res.status(500).json({ success: false, error });
     }
   } else {
